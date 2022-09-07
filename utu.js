a9d7a0b8711c6e060e251686566870b15651deb1
imad047 <35015501+imad047@users.noreply.github.com>
2022-09-07 22:26:58 +0100

    Create utu.js

---
+++ utu.js
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery.lazyloading demo</title>
+  
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script>/*<![CDATA[*/ 
+var url = "https://imad047.github.io/file/lazyloading.js";
+$.getScript( url, function() {
+if(lazyjs=='https://raw.githack.com/imad047/file/main/lazyding.html'){
+    alert('He is '+lazyjs);
+  }else{
+   alert('He is not '+lazyjs);
+  }  
+});
+/*]]>*/</script>
+</head>
+<body>
+
+ 
+</body>
+</html>
